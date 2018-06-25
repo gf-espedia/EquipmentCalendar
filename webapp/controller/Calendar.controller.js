08e4d2f5b785d8826c2482902f7ae2222e73a0ab
P2000114125 <giovanni.visona@espedia.it>
2018-06-25 10:16:20 +0000

    Added cycle operations

--- webapp/controller/Calendar.controller.js
+++ webapp/controller/Calendar.controller.js
@@ -10,7 +10,7 @@ sap.ui.define([
 			this._cModel = new sap.ui.model.json.JSONModel({
 				"Data": []
 			});
-			
+
 			this.getView().byId("calendar").setBuiltInViews(["Day", "Week", "Month", "One Month"]);
 
 			this.getOwnerComponent().getModel().attachMetadataLoaded(null, this.updateCalendar, this);
@@ -125,7 +125,7 @@ sap.ui.define([
 							appointment.MaintItemDescruption = entry.MaintItemDescruption;
 							appointment.type = (entry.WorkOrder && entry.WorkOrder.length > 0) ? "Type06" : "Type04";
 							appointment.WorkOrder = entry.WorkOrder;
-							
+
 							appointment.CallHorizon = entry.CallHorizon;
 							appointment.SchedulingPeriod = entry.SchedulingPeriod;
 							appointment.SchedulingPeriodUnit = entry.SchedulingPeriodUnit;
@@ -134,7 +134,6 @@ sap.ui.define([
 							appointment.GroupCounter = entry.GroupCounter;
 							appointment.TaskListDescription = entry.TaskListDescription;
 
-							
 							entity.Appointments = [appointment];
 							data.push(entity);
 						}
@@ -151,27 +150,57 @@ sap.ui.define([
 
 		handleAppointmentSelect: function(oEvent) {
 			var oAppointment = oEvent.getParameter("appointment");
-			var sPath = oEvent.getParameter("appointment").getBindingContext().getPath();
-			//var selectedRow = oEvent.getParameter("appointment").getParent().getBindingContext().getPath();
-			this.getView().byId("maintPlanForm").bindElement(sPath);
-			var appointmentData = this._cModel.getProperty(sPath);
-			if (appointmentData.WorkOrder && appointmentData.WorkOrder.length > 0){
-				this.getView().byId("maintOrderForm").setVisible(true);
-			this.getView().byId("maintOrderForm").bindElement("/OrderSet('" + appointmentData.WorkOrder +"')");     
-			} else {
-				this.getView().byId("maintOrderForm").setVisible(false);
-			}
+
+			// If the appointment selected is a single event
 			if (oAppointment) {
-				this.getView().byId("DynamicSideContent").setShowSideContent(true);
-				sap.m.MessageToast.show("Appointment selected: " + oAppointment.getTitle() + "\n" + oAppointment.getText());
+				//this.getView().byId("DynamicSideContent").setShowSideContent(true);
+				this.setShowSideContent(true);
+				//sap.m.MessageToast.show("Appointment selected: " + oAppointment.getTitle() + "\n" + oAppointment.getText());
+
+				var sPath = oEvent.getParameter("appointment").getBindingContext().getPath();
+
+				//var selectedRow = oEvent.getParameter("appointment").getParent().getBindingContext().getPath();
+				this.getView().byId("maintPlanForm").bindElement(sPath);
+				var appointmentData = this._cModel.getProperty(sPath);
+				// Manage work orders data
+				if (appointmentData.WorkOrder && appointmentData.WorkOrder.length > 0) {
+					this.getView().byId("maintOrderForm").setVisible(true);
+					this.getView().byId("maintOrderForm").bindElement("/OrderSet('" + appointmentData.WorkOrder + "')");
+				} else {
+					this.getView().byId("maintOrderForm").setVisible(false);
+				}
+
+				// Manage Task List data
+				if (appointmentData.TaskListGroup && appointmentData.TaskListGroup.length > 0 && appointmentData.TaskListType && appointmentData.TaskListType
+					.length > 0 && appointmentData.GroupCounter && appointmentData.GroupCounter.length > 0) {
+					this.getView().byId("operationsList").setVisible(true);
+					this.getView().byId("cycleFormEntry").setVisible(true);
+					var lTemplate = new sap.m.StandardListItem({
+						"title": "{Ltxa1}",
+						"description": "{Vornr}"
+					});
+					var operationsPath = "/TaskListSet(Plnal='" +
+						appointmentData.GroupCounter + "',Plnty='" + appointmentData.TaskListType + "',Plnnr='" +
+						appointmentData.TaskListGroup + "')/GetTaskListOperationSet";
+					this.getView().byId("operationsList").bindItems(operationsPath, lTemplate);
+					// /TaskListSet(Plnal='1',Plnty='A',Plnnr='2')/GetTaskListOperationSet
+				} else {
+					this.getView().byId("operationsList").setVisible(false);
+					this.getView().byId("cycleFormEntry").setVisible(false);
+					
+				}
 			} else {
-				this.getView().byId("DynamicSideContent").setShowSideContent(false);
-				var aAppointments = oEvent.getParameter("appointments");
-				var sValue = aAppointments.length + " Appointments selected";
-				sap.m.MessageBox.show(sValue);
+				this.setShowSideContent(false); //getView().byId("DynamicSideContent").setShowSideContent(false);
+				/*	var aAppointments = oEvent.getParameter("appointments");
+					var sValue = aAppointments.length + " Appointments selected";
+					sap.m.MessageBox.show(sValue);*/
 			}
 		},
-		
+
+		setShowSideContent: function(bool) {
+			this.getView().byId("DynamicSideContent").setShowSideContent(bool);
+		},
+
 		hideSideContent: function() {
 			this.getView().byId("DynamicSideContent").setShowSideContent(false);
 		}
